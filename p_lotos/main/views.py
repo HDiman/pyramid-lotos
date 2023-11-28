28535407883eac8fe69c4369be3eb1049da91827
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-11-29 00:33:56 +0300

    Get is enough

--- p_lotos/main/views.py
+++ p_lotos/main/views.py
@@ -1,30 +1,25 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Genre
 
-# Create your views here.
 
-# Rename objects
+# Create your views here.
 def rename(request):
     obj = get_object_or_404(Genre, name='Samanta')
     obj.name = 'Sandra'
     obj.save()
     return redirect('home')
 
-# Rename objects
+
 def delete(request):
     obj = Genre.objects.get(pk=7)
     obj.delete()
     return redirect('home')
 
 
-
 def show_genres(request):
-    obj = Genre.objects.get(pk=7)
-    obj.delete()
-
-    # children = samanta.get_children()
-
-
+    # obj = Genre.objects.get(pk=3)
+    # obj.name = 'Metal Rock'
+    # obj.save()
 
     data = {
         'genres': Genre.objects.all(),
