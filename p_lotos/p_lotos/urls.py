0167577a3f4f26dcf6665756b9061eda2f5b6c49
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-11-28 20:29:58 +0300

    Create Admin

--- p_lotos/p_lotos/urls.py
+++ p_lotos/p_lotos/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^genres/$', include('main.show_genres')),
+    path('', include('main.urls')),
 ]
