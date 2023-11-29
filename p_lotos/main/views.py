9424f0c57d219a139eb0cdb84904eb2dd3094b94
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-11-29 22:13:29 +0300

    Work with create

--- p_lotos/main/views.py
+++ p_lotos/main/views.py
@@ -2,17 +2,28 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Genre
 
 
-# Create your views here.
+# Create your views here
+
+def create(request, id):
+    if request.method == 'POST':
+        return redirect('home')
+    else:
+        data = {
+            'genres': Genre.objects.all(),
+        }
+        return render(request, "create.html", context=data)
+
+
 def rename(request):
-    obj = get_object_or_404(Genre, name='Samanta')
-    obj.name = 'Sandra'
-    obj.save()
+    # obj = get_object_or_404(Genre, name='Samanta')
+    # obj.name = 'Sandra'
+    # obj.save()
     return redirect('home')
 
 
 def delete(request):
-    obj = Genre.objects.get(pk=7)
-    obj.delete()
+    # obj = Genre.objects.get(pk=7)
+    # obj.delete()
     return redirect('home')
 
 
