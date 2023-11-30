1a2db921ea68a18fb307802627b528322a0ade5e
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-11-30 21:50:40 +0300

    Board is loaded. Now is time to fill it with items.

--- p_lotos/main/urls.py
+++ p_lotos/main/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_genres, name="home"),
+    path('', views.index, name="home"),
+    path('edit/<int:id>/', views.edit, name="edit"),
     path('create', views.create, name="create"),
     path('rename', views.rename, name="rename"),
     path('delete', views.delete, name="delete"),
+    path('show_genres', views.show_genres, name="show_genres"),
 ]
--- p_lotos/main/views.py
+++ p_lotos/main/views.py
@@ -9,7 +9,7 @@ def create(request):
         if len(child) != 0:
             obj = Genre.objects.get(pk=parent)
             Genre.objects.create(name=child, parent=obj)
-        return redirect('home')
+        return redirect('show_genres')
     else:
         data = {'genres': Genre.objects.all()}
         return render(request, "create.html", context=data)
@@ -23,7 +23,7 @@ def rename(request):
             obj = Genre.objects.get(pk=item)
             obj.name = new_name
             obj.save()
-        return redirect('home')
+        return redirect('show_genres')
     else:
         data = {'genres': Genre.objects.all()}
         return render(request, "rename.html", context=data)
@@ -34,7 +34,7 @@ def delete(request):
         del_item = request.POST.get('item')
         obj = Genre.objects.get(pk=del_item)
         obj.delete()
-        return redirect('home')
+        return redirect('show_genres')
     else:
         data = {'genres': Genre.objects.all()}
         return render(request, "delete.html", context=data)
@@ -43,3 +43,69 @@ def delete(request):
 def show_genres(request):
     data = {'genres': Genre.objects.all()}
     return render(request, "genres.html", context=data)
+
+
+
+
+
+
+
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# Here is starting to move DB to Board
+
+def edit(request, id):
+    grid = Genre.objects.get(id=id)
+    if request.method == 'POST':
+        # form = request.POST
+        userform_0 = request.POST.get('item_0')
+        userform_1 = request.POST.get('item_1')
+        userform_2 = request.POST.get('item_2')
+        userform_3 = request.POST.get('item_3')
+        userform_4 = request.POST.get('item_4')
+        userform_5 = request.POST.get('item_5')
+        userform_6 = request.POST.get('item_6')
+        userform_7 = request.POST.get('item_7')
+        userform_8 = request.POST.get('item_8')
+
+        if len(userform_0) != 0:
+            grid.item_0 = userform_0
+        if len(userform_1) != 0:
+            grid.item_1 = userform_1
+        if len(userform_2) != 0:
+            grid.item_2 = userform_2
+        if len(userform_3) != 0:
+            grid.item_3 = userform_3
+        if len(userform_4) != 0:
+            grid.item_4 = userform_4
+        if len(userform_5) != 0:
+            grid.item_5 = userform_5
+        if len(userform_6) != 0:
+            grid.item_6 = userform_6
+        if len(userform_7) != 0:
+            grid.item_7 = userform_7
+        if len(userform_8) != 0:
+            grid.item_8 = userform_8
+        grid.save()
+        return redirect('home')
+    else:
+        data = {
+            'id': id,
+            'grid_0': Genre.objects.all()[0],
+            'grid_1': Genre.objects.all()[1],
+            'grid_2': Genre.objects.all()[2],
+            'grid_3': Genre.objects.all()[3],
+            'grid_4': Genre.objects.all()[4],
+            'grid_5': Genre.objects.all()[5],
+            'grid_6': Genre.objects.all()[6],
+            'grid_7': Genre.objects.all()[7],
+            'grid_8': Genre.objects.all()[8],
+        }
+        return render(request, 'edit.html', context=data)
+
+
+def index(request):
+    data = {
+        'id': id,
+        'genres': Genre.objects.all()
+    }
+    return render(request, 'index.html', context=data)
