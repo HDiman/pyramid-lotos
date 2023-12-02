9a63a5f274e96ccf3240ff0762da228e0fb045f0
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-12-02 20:57:26 +0300

    Work with Edit

--- p_lotos/main/views.py
+++ p_lotos/main/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Genre
 
 
-def eight(id):
-    item0_0 = Genre.objects.get(pk=id)
-    item0_0_children = item0_0.get_children()
-    item_num = len(item0_0_children)
-    iter_num = 8 - item_num
-    for i in range(iter_num):
-        Genre.objects.create(name='text', parent=item0_0)
-    return len(item0_0.get_children())
+def to_fill(id):
+    item = Genre.objects.get(pk=id)
+    item_children = item.get_children()
+    item_num = len(item_children)
+    if item_num < 8:
+        num = 8 - item_num
+        for i in range(num):
+            Genre.objects.create(name='-', parent=item)
 
 
 def create(request):
@@ -61,9 +61,8 @@ def show_genres(request):
 # Here is starting to move DB to Board
 
 def edit(request, id):
-    grid = Genre.objects.get(id=id)
+    # grid = Genre.objects.get(id=id)
     if request.method == 'POST':
-        # form = request.POST
         userform_0 = request.POST.get('item_0')
         userform_1 = request.POST.get('item_1')
         userform_2 = request.POST.get('item_2')
@@ -95,23 +94,212 @@ def edit(request, id):
         grid.save()
         return redirect('home')
     else:
+        id_now = 30
+        item0_0 = Genre.objects.get(pk=id_now)
+        item0_0_child = item0_0.get_children()
+        item0_1 = item0_0_child[0]
+        item0_2 = item0_0_child[1]
+        item0_3 = item0_0_child[2]
+        item0_4 = item0_0_child[3]
+        item0_5 = item0_0_child[4]
+        item0_6 = item0_0_child[5]
+        item0_7 = item0_0_child[6]
+        item0_8 = item0_0_child[7]
+        to_fill(item0_1.id)
+        to_fill(item0_2.id)
+        to_fill(item0_3.id)
+        to_fill(item0_4.id)
+        to_fill(item0_5.id)
+        to_fill(item0_6.id)
+        to_fill(item0_7.id)
+        to_fill(item0_8.id)
+
+        item1_0_child = item0_1.get_children()
+        item1_0 = item0_1
+        item1_1 = item1_0_child[0]
+        item1_2 = item1_0_child[1]
+        item1_3 = item1_0_child[2]
+        item1_4 = item1_0_child[3]
+        item1_5 = item1_0_child[4]
+        item1_6 = item1_0_child[5]
+        item1_7 = item1_0_child[6]
+        item1_8 = item1_0_child[7]
+
+        item2_0_child = item0_2.get_children()
+        item2_0 = item0_2
+        item2_1 = item2_0_child[0]
+        item2_2 = item2_0_child[1]
+        item2_3 = item2_0_child[2]
+        item2_4 = item2_0_child[3]
+        item2_5 = item2_0_child[4]
+        item2_6 = item2_0_child[5]
+        item2_7 = item2_0_child[6]
+        item2_8 = item2_0_child[7]
+
+        item3_0_child = item0_3.get_children()
+        item3_0 = item0_3
+        item3_1 = item3_0_child[0]
+        item3_2 = item3_0_child[1]
+        item3_3 = item3_0_child[2]
+        item3_4 = item3_0_child[3]
+        item3_5 = item3_0_child[4]
+        item3_6 = item3_0_child[5]
+        item3_7 = item3_0_child[6]
+        item3_8 = item3_0_child[7]
+
+        item4_0_child = item0_4.get_children()
+        item4_0 = item0_4
+        item4_1 = item4_0_child[0]
+        item4_2 = item4_0_child[1]
+        item4_3 = item4_0_child[2]
+        item4_4 = item4_0_child[3]
+        item4_5 = item4_0_child[4]
+        item4_6 = item4_0_child[5]
+        item4_7 = item4_0_child[6]
+        item4_8 = item4_0_child[7]
+
+        item5_0_child = item0_5.get_children()
+        item5_0 = item0_5
+        item5_1 = item5_0_child[0]
+        item5_2 = item5_0_child[1]
+        item5_3 = item5_0_child[2]
+        item5_4 = item5_0_child[3]
+        item5_5 = item5_0_child[4]
+        item5_6 = item5_0_child[5]
+        item5_7 = item5_0_child[6]
+        item5_8 = item5_0_child[7]
+
+        item6_0_child = item0_6.get_children()
+        item6_0 = item0_6
+        item6_1 = item6_0_child[0]
+        item6_2 = item6_0_child[1]
+        item6_3 = item6_0_child[2]
+        item6_4 = item6_0_child[3]
+        item6_5 = item6_0_child[4]
+        item6_6 = item6_0_child[5]
+        item6_7 = item6_0_child[6]
+        item6_8 = item6_0_child[7]
+
+        item7_0_child = item0_7.get_children()
+        item7_0 = item0_7
+        item7_1 = item7_0_child[0]
+        item7_2 = item7_0_child[1]
+        item7_3 = item7_0_child[2]
+        item7_4 = item7_0_child[3]
+        item7_5 = item7_0_child[4]
+        item7_6 = item7_0_child[5]
+        item7_7 = item7_0_child[6]
+        item7_8 = item7_0_child[7]
+
+        item8_0_child = item0_8.get_children()
+        item8_0 = item0_8
+        item8_1 = item8_0_child[0]
+        item8_2 = item8_0_child[1]
+        item8_3 = item8_0_child[2]
+        item8_4 = item8_0_child[3]
+        item8_5 = item8_0_child[4]
+        item8_6 = item8_0_child[5]
+        item8_7 = item8_0_child[6]
+        item8_8 = item8_0_child[7]
+
         data = {
             'id': id,
-            'grid_0': Genre.objects.all()[0],
-            'grid_1': Genre.objects.all()[1],
-            'grid_2': Genre.objects.all()[2],
-            'grid_3': Genre.objects.all()[3],
-            'grid_4': Genre.objects.all()[4],
-            'grid_5': Genre.objects.all()[5],
-            'grid_6': Genre.objects.all()[6],
-            'grid_7': Genre.objects.all()[7],
-            'grid_8': Genre.objects.all()[8],
-        }
+            'item0_0': item0_0,
+            'item0_1': item0_1,
+            'item0_2': item0_2,
+            'item0_3': item0_3,
+            'item0_4': item0_4,
+            'item0_5': item0_5,
+            'item0_6': item0_6,
+            'item0_7': item0_7,
+            'item0_8': item0_8,
+
+            'item1_0': item1_0,
+            'item1_1': item1_1,
+            'item1_2': item1_2,
+            'item1_3': item1_3,
+            'item1_4': item1_4,
+            'item1_5': item1_5,
+            'item1_6': item1_6,
+            'item1_7': item1_7,
+            'item1_8': item1_8,
+
+            'item2_0': item2_0,
+            'item2_1': item2_1,
+            'item2_2': item2_2,
+            'item2_3': item2_3,
+            'item2_4': item2_4,
+            'item2_5': item2_5,
+            'item2_6': item2_6,
+            'item2_7': item2_7,
+            'item2_8': item2_8,
+
+            'item3_0': item3_0,
+            'item3_1': item3_1,
+            'item3_2': item3_2,
+            'item3_3': item3_3,
+            'item3_4': item3_4,
+            'item3_5': item3_5,
+            'item3_6': item3_6,
+            'item3_7': item3_7,
+            'item3_8': item3_8,
+
+            'item4_0': item4_0,
+            'item4_1': item4_1,
+            'item4_2': item4_2,
+            'item4_3': item4_3,
+            'item4_4': item4_4,
+            'item4_5': item4_5,
+            'item4_6': item4_6,
+            'item4_7': item4_7,
+            'item4_8': item4_8,
+
+            'item5_0': item5_0,
+            'item5_1': item5_1,
+            'item5_2': item5_2,
+            'item5_3': item5_3,
+            'item5_4': item5_4,
+            'item5_5': item5_5,
+            'item5_6': item5_6,
+            'item5_7': item5_7,
+            'item5_8': item5_8,
+
+            'item6_0': item6_0,
+            'item6_1': item6_1,
+            'item6_2': item6_2,
+            'item6_3': item6_3,
+            'item6_4': item6_4,
+            'item6_5': item6_5,
+            'item6_6': item6_6,
+            'item6_7': item6_7,
+            'item6_8': item6_8,
+
+            'item7_0': item7_0,
+            'item7_1': item7_1,
+            'item7_2': item7_2,
+            'item7_3': item7_3,
+            'item7_4': item7_4,
+            'item7_5': item7_5,
+            'item7_6': item7_6,
+            'item7_7': item7_7,
+            'item7_8': item7_8,
+
+            'item8_0': item8_0,
+            'item8_1': item8_1,
+            'item8_2': item8_2,
+            'item8_3': item8_3,
+            'item8_4': item8_4,
+            'item8_5': item8_5,
+            'item8_6': item8_6,
+            'item8_7': item8_7,
+            'item8_8': item8_8,
+            }
         return render(request, 'edit.html', context=data)
 
 
 def index(request):
     id = 30
+    to_fill(id)
     item0_0 = Genre.objects.get(pk=id)
     item0_0_child = item0_0.get_children()
     item0_1 = item0_0_child[0]
@@ -122,6 +310,14 @@ def index(request):
     item0_6 = item0_0_child[5]
     item0_7 = item0_0_child[6]
     item0_8 = item0_0_child[7]
+    to_fill(item0_1.id)
+    to_fill(item0_2.id)
+    to_fill(item0_3.id)
+    to_fill(item0_4.id)
+    to_fill(item0_5.id)
+    to_fill(item0_6.id)
+    to_fill(item0_7.id)
+    to_fill(item0_8.id)
 
     item1_0_child = item0_1.get_children()
     item1_0 = item0_1
@@ -134,95 +330,172 @@ def index(request):
     item1_7 = item1_0_child[6]
     item1_8 = item1_0_child[7]
 
+    item2_0_child = item0_2.get_children()
+    item2_0 = item0_2
+    item2_1 = item2_0_child[0]
+    item2_2 = item2_0_child[1]
+    item2_3 = item2_0_child[2]
+    item2_4 = item2_0_child[3]
+    item2_5 = item2_0_child[4]
+    item2_6 = item2_0_child[5]
+    item2_7 = item2_0_child[6]
+    item2_8 = item2_0_child[7]
+
+    item3_0_child = item0_3.get_children()
+    item3_0 = item0_3
+    item3_1 = item3_0_child[0]
+    item3_2 = item3_0_child[1]
+    item3_3 = item3_0_child[2]
+    item3_4 = item3_0_child[3]
+    item3_5 = item3_0_child[4]
+    item3_6 = item3_0_child[5]
+    item3_7 = item3_0_child[6]
+    item3_8 = item3_0_child[7]
+
+    item4_0_child = item0_4.get_children()
+    item4_0 = item0_4
+    item4_1 = item4_0_child[0]
+    item4_2 = item4_0_child[1]
+    item4_3 = item4_0_child[2]
+    item4_4 = item4_0_child[3]
+    item4_5 = item4_0_child[4]
+    item4_6 = item4_0_child[5]
+    item4_7 = item4_0_child[6]
+    item4_8 = item4_0_child[7]
+
+    item5_0_child = item0_5.get_children()
+    item5_0 = item0_5
+    item5_1 = item5_0_child[0]
+    item5_2 = item5_0_child[1]
+    item5_3 = item5_0_child[2]
+    item5_4 = item5_0_child[3]
+    item5_5 = item5_0_child[4]
+    item5_6 = item5_0_child[5]
+    item5_7 = item5_0_child[6]
+    item5_8 = item5_0_child[7]
+
+    item6_0_child = item0_6.get_children()
+    item6_0 = item0_6
+    item6_1 = item6_0_child[0]
+    item6_2 = item6_0_child[1]
+    item6_3 = item6_0_child[2]
+    item6_4 = item6_0_child[3]
+    item6_5 = item6_0_child[4]
+    item6_6 = item6_0_child[5]
+    item6_7 = item6_0_child[6]
+    item6_8 = item6_0_child[7]
+
+    item7_0_child = item0_7.get_children()
+    item7_0 = item0_7
+    item7_1 = item7_0_child[0]
+    item7_2 = item7_0_child[1]
+    item7_3 = item7_0_child[2]
+    item7_4 = item7_0_child[3]
+    item7_5 = item7_0_child[4]
+    item7_6 = item7_0_child[5]
+    item7_7 = item7_0_child[6]
+    item7_8 = item7_0_child[7]
+
+    item8_0_child = item0_8.get_children()
+    item8_0 = item0_8
+    item8_1 = item8_0_child[0]
+    item8_2 = item8_0_child[1]
+    item8_3 = item8_0_child[2]
+    item8_4 = item8_0_child[3]
+    item8_5 = item8_0_child[4]
+    item8_6 = item8_0_child[5]
+    item8_7 = item8_0_child[6]
+    item8_8 = item8_0_child[7]
+
     data = {
-        'item0_0': item0_0.name,
-        'item0_1': item0_1.name,
-        'item0_2': item0_2.name,
-        'item0_3': item0_3.name,
-        'item0_4': item0_4.name,
-        'item0_5': item0_5.name,
-        'item0_6': item0_6.name,
-        'item0_7': item0_7.name,
-        'item0_8': item0_8.name,
+        'item0_0': item0_0,
+        'item0_1': item0_1,
+        'item0_2': item0_2,
+        'item0_3': item0_3,
+        'item0_4': item0_4,
+        'item0_5': item0_5,
+        'item0_6': item0_6,
+        'item0_7': item0_7,
+        'item0_8': item0_8,
 
-        'item1_0': item1_0.name,
-        'item1_1': item1_1.name,
-        'item1_2': item1_2.name,
-        'item1_3': item1_3.name,
-        'item1_4': item1_4.name,
-        'item1_5': item1_5.name,
-        'item1_6': item1_6.name,
-        'item1_7': item1_7.name,
-        'item1_8': item1_8.name,
+        'item1_0': item1_0,
+        'item1_1': item1_1,
+        'item1_2': item1_2,
+        'item1_3': item1_3,
+        'item1_4': item1_4,
+        'item1_5': item1_5,
+        'item1_6': item1_6,
+        'item1_7': item1_7,
+        'item1_8': item1_8,
 
-        'item2_0': '-',
-        'item2_1': '-',
-        'item2_2': '-',
-        'item2_3': '-',
-        'item2_4': '-',
-        'item2_5': '-',
-        'item2_6': '-',
-        'item2_7': '-',
-        'item2_8': '-',
+        'item2_0': item2_0,
+        'item2_1': item2_1,
+        'item2_2': item2_2,
+        'item2_3': item2_3,
+        'item2_4': item2_4,
+        'item2_5': item2_5,
+        'item2_6': item2_6,
+        'item2_7': item2_7,
+        'item2_8': item2_8,
 
-        'item3_0': '-',
-        'item3_1': '-',
-        'item3_2': '-',
-        'item3_3': '-',
-        'item3_4': '-',
-        'item3_5': '-',
-        'item3_6': '-',
-        'item3_7': '-',
-        'item3_8': '-',
+        'item3_0': item3_0,
+        'item3_1': item3_1,
+        'item3_2': item3_2,
+        'item3_3': item3_3,
+        'item3_4': item3_4,
+        'item3_5': item3_5,
+        'item3_6': item3_6,
+        'item3_7': item3_7,
+        'item3_8': item3_8,
 
-        'item4_0': '-',
-        'item4_1': '-',
-        'item4_2': '-',
-        'item4_3': '-',
-        'item4_4': '-',
-        'item4_5': '-',
-        'item4_6': '-',
-        'item4_7': '-',
-        'item4_8': '-',
+        'item4_0': item4_0,
+        'item4_1': item4_1,
+        'item4_2': item4_2,
+        'item4_3': item4_3,
+        'item4_4': item4_4,
+        'item4_5': item4_5,
+        'item4_6': item4_6,
+        'item4_7': item4_7,
+        'item4_8': item4_8,
 
-        'item5_0': '-',
-        'item5_1': '-',
-        'item5_2': '-',
-        'item5_3': '-',
-        'item5_4': '-',
-        'item5_5': '-',
-        'item5_6': '-',
-        'item5_7': '-',
-        'item5_8': '-',
+        'item5_0': item5_0,
+        'item5_1': item5_1,
+        'item5_2': item5_2,
+        'item5_3': item5_3,
+        'item5_4': item5_4,
+        'item5_5': item5_5,
+        'item5_6': item5_6,
+        'item5_7': item5_7,
+        'item5_8': item5_8,
 
-        'item6_0': '-',
-        'item6_1': '-',
-        'item6_2': '-',
-        'item6_3': '-',
-        'item6_4': '-',
-        'item6_5': '-',
-        'item6_6': '-',
-        'item6_7': '-',
-        'item6_8': '-',
+        'item6_0': item6_0,
+        'item6_1': item6_1,
+        'item6_2': item6_2,
+        'item6_3': item6_3,
+        'item6_4': item6_4,
+        'item6_5': item6_5,
+        'item6_6': item6_6,
+        'item6_7': item6_7,
+        'item6_8': item6_8,
 
-        'item7_0': '-',
-        'item7_1': '-',
-        'item7_2': '-',
-        'item7_3': '-',
-        'item7_4': '-',
-        'item7_5': '-',
-        'item7_6': '-',
-        'item7_7': '-',
-        'item7_8': '-',
+        'item7_0': item7_0,
+        'item7_1': item7_1,
+        'item7_2': item7_2,
+        'item7_3': item7_3,
+        'item7_4': item7_4,
+        'item7_5': item7_5,
+        'item7_6': item7_6,
+        'item7_7': item7_7,
+        'item7_8': item7_8,
 
-        'item8_0': '-',
-        'item8_1': '-',
-        'item8_2': '-',
-        'item8_3': '-',
-        'item8_4': '-',
-        'item8_5': '-',
-        'item8_6': '-',
-        'item8_7': '-',
-        'item8_8': '-',
+        'item8_0': item8_0,
+        'item8_1': item8_1,
+        'item8_2': item8_2,
+        'item8_3': item8_3,
+        'item8_4': item8_4,
+        'item8_5': item8_5,
+        'item8_6': item8_6,
+        'item8_7': item8_7,
+        'item8_8': item8_8,
     }
     return render(request, 'index.html', context=data)
