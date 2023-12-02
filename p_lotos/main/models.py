dc72840d5cd706babb7418fa99f1a903695ca000
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-12-02 18:12:05 +0300

    Uniqueness is False

--- p_lotos/main/models.py
+++ p_lotos/main/models.py
@@ -3,7 +3,7 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Genre(MPTTModel):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, unique=False)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
     class MPTTMeta:
